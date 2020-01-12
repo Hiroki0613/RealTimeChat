c95f99ab93218ebf3e4a81c3b39c5b79fe45f81a
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-12 20:24:12 +0900

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by 宏輝 on 12/01/2020.
+//  Copyright © 2020 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
