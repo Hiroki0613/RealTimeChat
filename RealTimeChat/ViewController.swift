1c38fe048abe84574e335be7d9d2cb57a706f3c3
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-17 22:22:21 +0900

    messagesCollectionViewのdelegate、datasourceを実装

--- RealTimeChat/ViewController.swift
+++ RealTimeChat/ViewController.swift
@@ -9,13 +9,50 @@
 import UIKit
 import MessageKit
 
-class ViewController: MessagesViewController {
-
+class ViewController: MessagesViewController,MessagesDisplayDelegate,MessagesLayoutDelegate {
+    
+    
+    let sender = Sender(id: "any_unique_id", displayName: "Steven")
+    let messages: [MessageType] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        
+        
     }
-
-
+    
+    
 }
 
+extension ViewController: MessagesDataSource {
+    func currentSender() -> SenderType {
+        <#code#>
+    }
+    
+    // idとdisplayNameをプロパティに持つ構造体Senderを返す
+    func currentSender() -> Sender {
+        return Sender(id: "1", displayName: "Tom")
+    }
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    func messageForItem(at indexPath: IndexPath,
+                        in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    func cellTopLabelAttributedText(for message: MessageType,
+                                    at indexPath: IndexPath) -> NSAttributedString? {
+        let name = message.sender.displayName
+        return NSAttributedString(
+            string: name,
+            attributes: [
+                .font: UIFont.preferredFont(forTextStyle: .caption1),
+                .foregroundColor: UIColor(white: 0.3, alpha: 1)
+            ]
+        )
+    }
+}
